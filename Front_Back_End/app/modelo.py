de9e2067662e5f453829598c6e02c5d6954c5f3a
AxelXiaoMI <axelxiao482@gmail.com>
2025-11-15 20:29:44 -0300

    Terminacion de la pagina de formularios y casi terminar cobros

--- Front_Back_End/app/modelo.py
+++ Front_Back_End/app/modelo.py
@@ -9,8 +9,9 @@ app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'xiao'
 app.config['MYSQL_DB'] = 'ticket_airplane'
-
 conexion = MySQL(app)
+
+# app.secret_key = "llave_secreta"
 
 #Para poner una direccion hay que usar el @app.route("/") y luego de la / ponemos la URL que queramos
 @app.route("/")
@@ -40,17 +41,62 @@ def vuelos_destino(vuelos):
     precio = [fila[0] for fila in precio]
     return render_template("vuelos_destino.html", vuelos= vuelos, aerolinea= aerolinea, precio = precio)
 
-@app.route("/comprar_boleto/<vuelos>/<aerolinea>/<precio>")
-#ME QUEDE ACA, ESTOY HACIENDO LA CUENTA DE CUANTO DEBERIAN DE PAGAR AUN NO SE BIEN LOS CLACULOS TENGO QUE VER COMO ESTA EN DESPEGAR
+@app.route("/comprar_boleto/<vuelos>/<aerolinea>/<float:precio>", methods=['GET', 'POST'])
 def comprar_boleto(vuelos, aerolinea, precio):
+    total = None
+    cant_menores = None
+    adultos = None
+    edad_menores = []
+    boton_especial = False
+    act_calculo = False
     if request.method == "POST":
+        #Logica para el HTML para que este pueda repetir el codigo la cantidad de veces indicada en menores
         adultos = request.form['cantidad_personas_adultas']
-        menores = request.form['cantidad_personas_menores']
-        total = precio * adultos 
-    return render_template("comprar_boleto.html")
+        adultos = int(adultos)
+        cant_menores = request.form['cantidad_personas_menores']
+        cant_menores = int(cant_menores)
+        act_calculo = request.form['act_calculo']
+        boton_especial = True
+        if cant_menores == 0:
+            total = (precio * adultos) + (precio * 0.6 *cant_menores)
+            return redirect(url_for('calculo_total', vuelos = vuelos, aerolinea = aerolinea, total = total, cant_menores = cant_menores, adultos= adultos, precio = precio))
+        elif act_calculo == 'True':
+            if cant_menores != 0:    
+                for menor in range (cant_menores):
+                    edad_menores.append(int(request.form[f'edad_niños_{menor+1}']))
+                for edad in edad_menores:
+                    if edad >= 13:
+                        adultos += 1
+                        cant_menores -= 1
+            total = (precio * adultos) + (precio * 0.6 *cant_menores)
+            return redirect(url_for('calculo_total', vuelos = vuelos, aerolinea = aerolinea, total = total, cant_menores = cant_menores, adultos= adultos, precio = precio))
+    return render_template("comprar_boleto.html", total = total, cant_menores = cant_menores, adultos= adultos, boton_especial = boton_especial)
+
+@app.route("/calculo_total<vuelos><aerolinea><int:total><int:cant_menores><int:adultos>, <float:precio>")
+def calculo_total(vuelos, aerolinea, total, cant_menores, adultos, precio):
+    step = 1
+    if request.method == 'POST':
+        if step == 1:
+            cant_menores + adultos
+            nombre = request.form['nombre']
+            apellido = request.form['apellido']
+            pais_residencia = request.form['pais_residencia']
+            tipo_documento = request.form['tipo_documento']
+            num_documento = request.form['num_documento']
+            email = request.form['email']
+            fecha_nacimiento = request.form['fecha_nacimiento']
+            sexo = request.form['sexo']
+            seguro = request.form['seguro']
+            metodo_pago = request.form['metodo_pago']
+            step = request.form['step']
+            
+
+    return render_template("completar_datos.html", vuelos = vuelos, aerolinea = aerolinea, total = total, cant_menores = cant_menores, adultos = adultos, precio = precio, step = step)
 
 
-
+@app.route("/datos_seguros")
+def datos_seguros():
+    return render_template("datos_seguros.html")
 
 @app.route("/registrar_usuario", methods=['GET', 'POST'])
 def registrar_usuario():
